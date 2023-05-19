a61675731b5c5260556b29d8c63ee8b509ffc78c
zubayer22 <zubayerkh@gmail.com>
2023-05-19 15:53:22 +0600

    Vercel Added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -39,7 +39,12 @@ async function run() {
             res.send(result);
         })
 
-
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
 
 
 
